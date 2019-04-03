727c7cf6ebe071deede72a1d20e32f05760ffc16
Kannon Ford <c3163911@uon.edu.au>
2019-04-03 06:57:57 +0000

    first attempt

--- assign_1.c
+++ assign_1.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+#include<stdio.h>
+// define functions
+//function prototype for encrypting rotation cypher
+char encrytpRotate (int *message, int key)
+//function prototype for decrypting rotation cypher 
+//function prototype for encrypting sub cypher
+//function prototype for decrypting sub cypher 
 
-
-int main{
+int main() {
     
-    printf("wahahhahahahhaaSSDZZZZAAA");
+   //messageText = abcdefg
+   //rotationAmount = 2
+
+    
 }
+
+//function definition for encrypting rotation cypher
+char encrytpRotate (int *message, int key)  {
+    char message[50] ; //array of message 50 long 
+    char messLett ; //one letter from message
+    int i ; //counter
+    int key ; //key
+    
+    for (i=0 ; i < message.length ; i++) {
+        messLett = (messlett*k)*(%26) ;
+        newLett = messLett ;
+                  
+     }
+    return newLett ; 
+    
+    }
+        
+//function definition for decrypting rotation cypher 
+//function definition for encrypting sub cypher
+//function definition for decrypting sub cypher 
