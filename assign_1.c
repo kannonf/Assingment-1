4b34423f40d9c7aeb63506cb5bf46366f19b9fee
Kannon Ford <c3163911@uon.edu.au>
2019-04-10 08:27:21 +0000

    decryptrotate working

--- assign_1.c
+++ assign_1.c
@@ -2,20 +2,20 @@
 
 // define functions  // CHECK ISSPACE function
 //function prototype for encrypting rotation cypher
-char encryptRotate (char *input, int k, unsigned long int str_len) ;
+//    DONE: char encryptRotate (char *input, int k, unsigned long int strLen) ;
 //function prototype for decrypting rotation cypher 
-//char unencryptRotate (char input, int k)
+//    DONE: char unencryptRotate (char *input, int k, unsigned long int strLen ) ;
 //function prototype for encrypting sub cypher
-//char encryptSub (chr input, int k)
+char encryptSub (chr input, int k)
 //function prototype for decrypting sub cypher 
 //char unencryptSub (chr input, int k)
 //extra comments
 int main() {
     
-    char input[] = "Hello I'm writing ___ A TesT S^^tring" ;
+    char input[] = "Hello I'm writing ___ A TesT S^^tring zzzzzzzzzzzzzzzz" ;
     printf("message: %s\n", input);
-    unsigned long int str_len = (sizeof(input)/sizeof(char));
-    encryptRotate(input, 20, str_len);
+    unsigned long int strLen = (sizeof(input)/sizeof(char));
+    unencryptRotate(input, 1, strLen );                          //middle letter is key fro rotataon cyphers
     printf("Output: \n");
     for (int j = 0; j < (sizeof(input)/sizeof(char)); j++) {
         printf("%c", input[j]);
@@ -30,36 +30,68 @@ int main() {
  }      
 
 
-
+/* DONE ___________________________________________________________________________
 //function definition for encrypting rotation cypher
-char encryptRotate (char *input, int k, unsigned long int str_len) {
+char encryptRotate (char *input, int k, unsigned long int strLen ) {
     int i = 0 ; //counter
     
     printf("Input is %lu chars long\n", str_len);
-    for (i = 0 ; i < str_len; i++) {
+    for (i = 0 ; i < strLen ; i++) {
             
-         //Convert LC to UC 
+        //Convert LC to UC 
         if (input[i] > 96 && input[i] < 123) {
-            printf("Input is LC: %c\n", input[i]);
-            input[i] = input[i] - 32; 
-            printf("Converted to UC: %c\n", input[i]);
+            printf("Input is LC: %c\n", input[i]) ;
+            input[i] = input[i] - 32 ; 
+            printf("Converted to UC: %c\n", input[i]) ;
         }
 
 
         //Use the encrypt function on UC letters
         if (input[i] > 64 && input[i] < 91) {
-            printf("About to encrypt: %c %d\n", input[i], input[i]);
-            input[i] = ((((int)input[i])+k)-65)%26; 
+            printf("About to encrypt: %c %d\n", input[i], input[i]) ;
+            input[i] = ((((int)input[i])+k)-65)%26 ; 
             input[i] += 65 ;
-            printf("Encrypted value: %c\n", input[i]);
-        }
-         
-               
-        //printf("About to encrypt: %c %d\n", input[], input[]);
-        //input[i] = (((int)input[i])+k)%90;          
+            printf("Encrypted value: %c\n", input[i]) ;
+        }          
+          
      }
 }
-         
-//function definition for decrypting rotation cypher 
+     
+//function definition for decrypting rotation cypher
+char unencryptRotate (char *input, int k, unsigned long int strLen ) {
+    int i = 0 ; //counter
+    
+    printf("Input is %lu chars long\n", strLen) ; // tracking string length 
+    for (i = 0 ; i < strLen ; i++) {
+        
+        //Convert LC to UC 
+        if (input[i] > 96 && input[i] < 123) {
+            printf("Input is LC: %c\n", input[i]) ;
+            input[i] = input[i] - 32; 
+            printf("Converted to UC: %c\n", input[i]) ;
+        }
+
+
+        //Use the encrypt function on UC letters
+        if (input[i] > 64 && input[i] < 91) {
+            printf("About to encrypt: %c %d\n", input[i], input[i]) ;
+            input[i] = ((((int)input[i])+k)-65)%26 ; 
+            input[i] += 65 ;
+            printf("Encrypted value: %c\n", input[i]) ;
+
+        }
+    }
+}
+  ________________________________________________________________________________*/ 
+
+
+
+
+
+
+
+
+
+
 //function definition for encrypting sub cypher
 //function definition for decrypting sub cypher 
